8f1230734a244505d41b1c6424b78bfe82e5f2cc
rumious <jamakinano@student.apc.edu.ph>
2026-01-28 15:47:56 +0800

    Create CurrencyService.java

---
+++ finals/CurrencyService.java
@@ -0,0 +1,10 @@
+package finals;
+public class CurrencyService {
+
+    public CurrencyService() {
+        //constructor for getting currency rates from an api
+    }
+    public float toConvert(/*String from, String to, */int amount) {
+        return amount+1;
+    }
+}
